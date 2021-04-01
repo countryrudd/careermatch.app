d09af645aa6b7f41fa34b9564b19653d41de4fc9
Craig White <craigwhiteofficial@gmail.com>
2021-03-31 23:24:33 -0400

    Add Vue.js project

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    lintOnSave: false,
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'DevLink';
+                return args;
+            });
+    }
+}
