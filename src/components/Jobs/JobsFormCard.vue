6658126bbad4cb8472b018abe7c30554f0bd9e62
Kevin Talbert <kevinbtalbert@gmail.com>
2021-04-11 16:28:07 -0400

    Added Jobs search page and views

---
+++ src/components/Jobs/JobsFormCard.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="card shadow">
+        <div class="card-body">
+            <form class="d-flex flex-column">
+                <fieldset>
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Search">
+                    </div>
+
+                    <h5 class="mt-3">Languages</h5>
+                    <div class="form-group d-flex flex-row align-items-center flex-wrap">
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-1">
+                            <label class="form-check-label" for="formCheck-1">Python</label>
+                        </div>
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-2">
+                            <label class="form-check-label" for="formCheck-2">JavaScript</label>
+                        </div>
+                        <button class="btn btn-link text-secondary" type="button">Add a language</button>
+                    </div>
+
+                    <h5 class="mt-3">Location</h5>
+                    <div class="form-group d-flex flex-row align-items-center flex-wrap">
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-3">
+                            <label class="form-check-label" for="formCheck-3">Cary, NC</label>
+                        </div>
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-4">
+                            <label class="form-check-label" for="formCheck-4">Charlotte, NC</label>
+                        </div>
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-5">
+                            <label class="form-check-label" for="formCheck-5">Raleigh, NC</label>
+                        </div>
+                        <button class="btn btn-link text-secondary" type="button">Add a location</button>
+                    </div>
+
+                    <h5 class="mt-3">Workplace Culture Tags</h5>
+                    <div class="form-group d-flex flex-row align-items-center flex-wrap">
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-6">
+                            <label class="form-check-label" for="formCheck-6">Fast-Paced Environment</label>
+                        </div>
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-7">
+                            <label class="form-check-label" for="formCheck-7">Startup</label>
+                        </div>
+                        <div class="form-check" style="margin-right: 15px;">
+                            <input class="form-check-input" type="checkbox" id="formCheck-8">
+                            <label class="form-check-label" for="formCheck-8">Task-Oriented</label>
+                        </div>
+                        <button class="btn btn-link text-secondary" type="button">Add a workplace culture tag</button>
+                    </div>
+
+
+                </fieldset>
+                <button class="btn btn-primary mt-4" type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'JobsFormCard'
+    }
+</script>
---
+++ src/components/Jobs/JobsListCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="card shadow">
+        <div class="card-body">
+            <div v-for="job in jobs" :key="job.id" class="row no-gutters mb-2">
+                <div class="col-2">
+                    <picture>
+                        <img :src="job.logo_url"
+                             :alt="job.name"
+                             style="max-height: 100%; max-width: 100%; display: block;">
+                    </picture>
+                </div>
+
+                <div class="col-10 pl-3">
+                    <router-link :to="{ name: 'Jobs' }" style="text-decoration: none;">
+                        <h5 class="mb-1">{{ job.name }}</h5>
+                    </router-link>
+
+                    <div class="d-flex">
+                        <strong class="me-2">Skills:</strong>
+                        <div class="d-flex flex-wrap">
+                            <span v-for="skill in job.skills" :key="skill" class="me-2">
+                                {{ skill }}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="d-flex">
+                        <strong class="me-2">Location:</strong>
+                        <span>{{ job.location }}</span>
+                    </div>
+
+                    <div class="d-flex">
+                        <strong class="me-2">Workplace-Culture Tags:</strong>
+                        <div class="d-flex flex-wrap">
+                            <span v-for="culture in job.culture" :key="culture" class="me-2">
+                                {{ culture }}
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'JobsListCard',
+        props: {
+            developers: { type: Array, required: true },
+        },
+    }
+</script>
