2746505551eb05eddc92d6d0292745a90058423d
Craig White <craigwhiteofficial@gmail.com>
2021-05-05 19:07:57 -0400

    Add docstring to CompanyService functions

--- src/services/CompanyService.js
+++ src/services/CompanyService.js
@@ -9,10 +9,21 @@ export const createCompany = (data) => {
     return axios.post('/companies/', data);
 };
 
+/**
+ * Gets an already existing Company.
+ *
+ * @param {number} id
+ * @param {Object} data
+ */
 export const updateCompany = (id, data) => {
     return axios.patch(`/companies/${id}/`, data);
 }
 
+/**
+ * Gets a Company.
+ *
+ * @param {number} id
+ */
 export const getCompany = (id) => {
     return axios.get(`/companies/${id}/`);
 };
